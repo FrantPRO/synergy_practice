1d90fcf5e6813826762cce08c7856a34d294954a
Stanislav Frantsuzov <stas.frantsuzov@gmail.com>
2025-01-01 20:45:33 +0100

    Semester_7_case_task_4 – updated backend

--- Semester_7_case_task_4/backend/app/main.py
+++ Semester_7_case_task_4/backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 from .routers.survey import router as surveys_router
+from .routers.response import router as responses_router
 from .routers.auth import router as auth_router, authentication, \
     create_admin_user, admin
 from .routers.user import router as user_router
@@ -35,6 +36,7 @@ app.add_middleware(
 app.include_router(surveys_router, dependencies=[Depends(authentication)])
 app.include_router(auth_router)
 app.include_router(user_router, dependencies=[Depends(admin)])
+app.include_router(responses_router, dependencies=[Depends(authentication)])
 
 def main():
     import uvicorn
